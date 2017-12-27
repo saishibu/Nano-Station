3631cf838ac1b216dce2069e96a974b697052cec
Sai Shibu <saishibu38380@gmail.com>
2017-12-27 15:35:38 +0530

    Update Datafetch.py

--- Datafetch.py
+++ Datafetch.py
@@ -4,20 +4,22 @@ import urllib, urllib2, cookielib,time,json
 
 user ='ubnt'
 pswd='amma'
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 cj=cookielib.CookieJar()
 opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-r=opener.open('http://192.168.1.20/login.cgi')
+r=opener.open('https://192.168.1.20/login.cgi')
 login_data=urllib.urlencode({'username':user, 'password':pswd,'action':'login'})
-r=opener.open('http://192.168.1.20/login.cgi',login_data)
+r=opener.open('https://192.168.1.20/login.cgi',login_data)
 
 while(1):
-	status_page=opener.open('http://192.168.1.20/status.cgi')
+	status_page=opener.open('https://192.168.1.20/status.cgi')
 	status=status_page.read()
 	print (status)
 	json_status=json.loads(status)
 	signal=json_status['wireless']['signal']
 	print str(signal) + 'db'
-	time.sleep(1)
+	time.sleep(5)
 	
 
