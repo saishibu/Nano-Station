f97baf8c1324441f5a707a1ca3f5010de2307575
saishibu38380 <saishibu38380@gmail.com>
2017-12-30 06:40:21 +0000

    OLED Display added

--- Datafetch.py
+++ Datafetch.py
@@ -3,18 +3,18 @@ import urllib, urllib2, cookielib,time,json
 
 
 user ='ubnt'
-pswd='amma'
+pswd='1234'
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 cj=cookielib.CookieJar()
 opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-r=opener.open('https://192.168.1.20/login.cgi')
+r=opener.open('https://192.168.179.100/login.cgi')
 login_data=urllib.urlencode({'username':user, 'password':pswd,'action':'login'})
-r=opener.open('https://192.168.1.20/login.cgi',login_data)
+r=opener.open('https://192.168.179.100/login.cgi',login_data)
 
 while(1):
-	status_page=opener.open('https://192.168.1.20/status.cgi')
+	status_page=opener.open('https://192.168.179.100/status.cgi')
 	status=status_page.read()
 	print (status)
 	json_status=json.loads(status)
---
+++ OLED_Display.py
@@ -0,0 +1,38 @@
+import time,Adafruit_SSD1306
+import Adafruit_GPIO.SPI as SPI
+
+import Image
+import ImageDraw
+import ImageFont
+
+
+RST =22
+DC=5
+SPI_PORT=0
+SPI_DEVICE=0
+
+disp= Adafruit_SSD1306.SSD1306_128_32(rst=RST,dc=DC,spi=SPI.SpiDev(SPI_PORT,SPI_DEVICE,max_speed_hz=8000000))
+
+disp.begin()
+disp.clear()
+disp.display()
+
+width=disp.width
+height=disp.height
+
+
+image =Image.new('1',(width,height))
+draw =ImageDraw.Draw(image)
+
+padding=1
+shape_width=20
+top=padding
+bottom=height-padding
+x=padding
+
+font = ImageFont.load_default()
+
+draw.text((x,top), 'Hello', font=font,fill=255)
+time.sleep(2)
+disp.image(image)
+disp.display()
